6c78857064508c6aead9a9ed25d1fa0ea8fdcc04
karan <app2.nessco@gmail.com>
2024-06-13 17:50:00 +0530

    sample

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 app.post('/indiamart/6dE+Ip6Xg+Qc6n5Pjm9OjErINbxsqtpw', (req, res) => {
-    res.send('API HIT');
+    console.log("api hit")
     // console.log(res)
     // console.log(req)
-    res.status(200).send('Data Received')
+    // res.status(200).send('Data Received')
+    res.send("API HIT")
 });
 app.post('/indiamart', (req, res) => {
     res.send('Indiamart');
-    console.log(res)
-    console.log(req)
-    res.json(res.status(200))
+    // console.log(res)
+    // console.log(req)
+    // res.json(res.status(200))
 });
 app.get('/', (req, res) => {
     res.send('API Working');
+    res.status(200)
 });
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on http://localhost:${process.env.PORT}`);
 });
