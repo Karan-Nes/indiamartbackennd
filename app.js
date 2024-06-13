e314fcaf01253ed774817d53227dd3a2eb452373
karan <app2.nessco@gmail.com>
2024-06-13 17:32:40 +0530

    changed

--- app.js
+++ app.js
@@ -2,8 +2,17 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get('/indiamart', (req, res) => {
+app.post('/indiamart/6dE+Ip6Xg+Qc6n5Pjm9OjErINbxsqtpw', (req, res) => {
+    res.send('API HIT');
+    // console.log(res)
+    // console.log(req)
+    res.status(200).send('Data Received')
+});
+app.post('/indiamart', (req, res) => {
     res.send('Indiamart');
+    console.log(res)
+    console.log(req)
+    res.json(res.status(200))
 });
 app.get('/', (req, res) => {
     res.send('API Working');
