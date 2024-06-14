d241d51a637436750ea5d0cdc623587f49c8ffc5
karan <app2.nessco@gmail.com>
2024-06-14 14:15:50 +0530

    changed

--- app.js
+++ app.js
@@ -2,20 +2,32 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
-// app.post('/indiamart', (req, res) => {
-//     res.send('Indiamart');
-//     // console.log(res)
-//     // console.log(req)
-//     // res.json(res.status(200))
-// });
+app.post('/indiamart', (req, res) => {
+    res.send('Indiamart');
+    // console.log(res)
+    // console.log(req)
+    // res.json(res.status(200))
+});
 console.log(encodeURIComponent("6dE+Ip6Xg+Qc6n5Pjm9OjErINbxsqtpw"))
 app.post(
   "/indiamart/" + encodeURIComponent("6dE+Ip6Xg+Qc6n5Pjm9OjErINbxsqtpw"),
   (req, res) => {
     res.send("hi");
     console.log("api hit")
+    res.status(200)
   }
 );
+// app.post(
+//   "/indiamart/6dE+Ip6Xg+Qc6n5Pjm9OjErINbxsqtpw",
+//   (req, res) => {
+//     res.send("hi");
+//     console.log("api hit")
+//     res.status(200)
+//   }
+// );
+
+
+
 app.get("/", (req, res) => {
   res.send("API Working");
   res.status(200);
