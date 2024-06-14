ef1642414869a55671ef04fa4d6ce6bf29a494a3
karan <app2.nessco@gmail.com>
2024-06-14 10:32:17 +0530

    added uri component

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
 
-app.post('/indiamart/6dE+Ip6Xg+Qc6n5Pjm9OjErINbxsqtpw', (req, res) => {
+// app.post('/indiamart', (req, res) => {
+//     res.send('Indiamart');
+//     // console.log(res)
+//     // console.log(req)
+//     // res.json(res.status(200))
+// });
+console.log(encodeURIComponent("6dE+Ip6Xg+Qc6n5Pjm9OjErINbxsqtpw"))
+app.get(
+  "/indiamart/" + encodeURIComponent("6dE+Ip6Xg+Qc6n5Pjm9OjErINbxsqtpw"),
+  (req, res) => {
+    res.send("hi");
     console.log("api hit")
-    // console.log(res)
-    // console.log(req)
-    // res.status(200).send('Data Received')
-    res.send("API HIT")
-});
-app.post('/indiamart', (req, res) => {
-    res.send('Indiamart');
-    // console.log(res)
-    // console.log(req)
-    // res.json(res.status(200))
-});
-app.get('/', (req, res) => {
-    res.send('API Working');
-    res.status(200)
+  }
+);
+app.get("/", (req, res) => {
+  res.send("API Working");
+  res.status(200);
 });
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server running on http://localhost:${process.env.PORT}`);
+  console.log(`Server running on http://localhost:${process.env.PORT}`);
 });
